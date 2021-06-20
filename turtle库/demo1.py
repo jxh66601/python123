2c919a89d7a9e2836887c8c18e2096bdf817e912
jxh66601 <597601816@qq.com>
2021-06-20 22:46:31 +0800

    lx3

---
+++ turtle库/demo1.py
@@ -0,0 +1,6 @@
+import turtle
+turtle.goto(100,100)
+turtle.goto(100,-100)
+turtle.goto(-100,-100)
+turtle.goto(-100,100)
+turtle.goto(0,0)
---
+++ turtle库/demo2.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.left(45)
+turtle.fd(150)
+turtle.right(135)
+turtle.fd(300)
+turtle.left(135)
+turtle.fd(150)
