a1583cfc6f7a646bd34fae66adde560be586a065
jxh66601 <597601816@qq.com>
2021-06-21 10:52:37 +0800

    lx5

---
+++ turtle库/demo4.py
@@ -0,0 +1,16 @@
+import turtle as t
+t.setup(650,350,200,200)
+t.penup()
+t.fd(-250)
+t.pendown()
+t.pensize(25)
+t.pencolor("purple")
+t.seth(-40)
+for i in range(4):
+    t.circle(40,80)
+    t.circle(-40,80)
+t.circle(40,80/2)
+t.fd(40)
+t.circle(16,180)
+t.fd(40*2/3)
+t.done()
