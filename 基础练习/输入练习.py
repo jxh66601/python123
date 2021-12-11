412356c100b187f43e7392252f9abae865d39c48
september <597601816@qq.com>
2021-12-11 13:52:48 +0800

    Changes

--- 基础练习/输入练习.py
+++ 基础练习/输入练习.py
@@ -1,4 +1,4 @@
-#在一行中输入三个用空格分隔的数
-a,b,c=map(int,input().split())
-d=max(a,b,c)
+# 在一行中输入三个用空格分隔的数
+a, b, c = map(int, input().split())
+d = max(a, b, c)
 print(d)
---
+++ 字符串/奇数数列求和.py
@@ -0,0 +1,5 @@
+n = int(input())
+sum = 0
+for i in range(1,2*n,2):
+    sum = sum + i
+print(sum)
